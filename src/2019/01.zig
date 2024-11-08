a3dea4e9495c10b6aad40d9ca3becbedda4f8a78
nick-ng <nick.ng@fergus.com>
2024-11-08 16:57:05 +1300

    wip: choose question based on filename

--- src/2019/01.zig
+++ src/2019/01.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const utils = @import("../utils.zig");
 
 pub fn answer() !void {
+    std.debug.print("Fuel requirements\n", .{});
     const allocator = std.heap.page_allocator;
     const file_content = try utils.readFile("./data/2019-01.txt", allocator);
     defer allocator.free(file_content);
@@ -13,8 +14,6 @@ pub fn answer() !void {
     var i: u32 = 0;
     while (i < file_content.len) : (i += 1) {
         if (file_content[i] == '\n') {
-            // std.debug.print("\n{s}, new line!\n", .{word_buffer[0..word_counter]});
-
             const word = word_buffer[0..word_counter];
             word_counter = 0;
             const number = try std.fmt.parseInt(i32, word, 10);
@@ -24,13 +23,11 @@ pub fn answer() !void {
             continue;
         }
 
-        // std.debug.print("{c}", .{file_content[i]});
         word_buffer[word_counter] = file_content[i];
         word_counter += 1;
     }
 
     const numbers = numbers_array[0..number_counter];
-    // std.debug.print("{any}\n", .{numbers});
 
     // part 1
     i = 0;
@@ -40,7 +37,7 @@ pub fn answer() !void {
         fuel_requirement_part_1 += temp;
     }
 
-    std.debug.print("Fuel requirement part 1: {}\n", .{fuel_requirement_part_1});
+    std.debug.print("Part 1: {}\n", .{fuel_requirement_part_1});
 
     // part 2
     i = 0;
@@ -50,7 +47,7 @@ pub fn answer() !void {
         fuel_requirement_part_2 += temp;
     }
 
-    std.debug.print("Fuel requirement part 2: {}\n", .{fuel_requirement_part_2});
+    std.debug.print("Part 2: {}\n", .{fuel_requirement_part_2});
 }
 
 fn calculateFuel(mass: i32) i32 {
--- src/main.zig
+++ src/main.zig
@@ -6,14 +6,24 @@ pub fn main() !void {
     var args_iter = std.process.args();
 
     _ = args_iter.skip();
-    const file_name = args_iter.next();
+    const temp_filename = args_iter.next();
 
-    if (file_name == null) {
+    if (temp_filename == null) {
         std.debug.print("no input file given\n", .{});
         return;
     }
 
     // 10. determine year and day based on filename
+    const filename = temp_filename.?;
+    std.debug.print("filename: {?s}\n", .{filename});
+    var year: u32 = 0;
+    var day: u8 = 0;
+    var word: [20]u8 = undefined;
+    var word_counter = 0;
+    var i: u32 = 0;
+    while (i < filename.len) : (i += 1) {
+        if (filename[i] == '.' or filename[i] == '/') {}
+    }
 
     // 20. run function for year and day
     try aoc2019day1.answer();
