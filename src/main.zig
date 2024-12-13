777199372f4abe2b4ff95b1168b70b627751ea15
nick-ng <nick.ng@fergus.com>
2024-12-13 14:14:48 +1300

    feat: detect question from input filename

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 const aoc2019day1 = @import("./2019/01.zig");
 
+const digits: [10]u8 = .{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
 pub fn main() !void {
-    std.debug.print("hello world\n", .{});
     var args_iter = std.process.args();
 
     _ = args_iter.skip();
@@ -19,12 +20,54 @@ pub fn main() !void {
     var year: u32 = 0;
     var day: u8 = 0;
     var word: [20]u8 = undefined;
-    var word_counter = 0;
+    var word_counter: u32 = 0;
     var i: u32 = 0;
     while (i < filename.len) : (i += 1) {
-        if (filename[i] == '.' or filename[i] == '/') {}
+        const character = filename[i];
+        if (character == '.' or character == '/' or character == '-') {
+            if (word_counter == 0) {
+                continue;
+            }
+
+            const temp1: []u8 = word[0..word_counter];
+            word_counter = 0;
+            if (year == 0) {
+                year = try std.fmt.parseInt(u32, temp1, 10);
+            } else if (day == 0) {
+                day = try std.fmt.parseInt(u8, temp1, 10);
+            }
+
+            continue;
+        }
+
+        var j: u32 = 0;
+        while (j < digits.len) : (j += 1) {
+            if (character == digits[j]) {
+                word[word_counter] = character;
+                word_counter += 1;
+            }
+        }
     }
 
+    std.debug.print("{}, day {}\n", .{ year, day });
+
     // 20. run function for year and day
-    try aoc2019day1.answer();
+    switch (year) {
+        2019 => {
+            switch (day) {
+                1 => {
+                    try aoc2019day1.answer();
+                    return;
+                },
+                else => {
+                    std.debug.print("No solution for {}, day {}\n", .{ year, day });
+                    return;
+                },
+            }
+        },
+        else => {
+            std.debug.print("No solutions for {}\n", .{year});
+            return;
+        },
+    }
 }
